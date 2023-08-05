675c20f7affabef48963463c0ed51b000e06f31f
LiteeCreates <motomart2018@gmail.com>
2023-08-05 10:58:30 +0700

    Items tab registry changed

--- src/main/java/net/linearmatrix/fetchini/item/ModCreativeModeTabs.java
+++ src/main/java/net/linearmatrix/fetchini/item/ModCreativeModeTabs.java
@@ -11,7 +11,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS=DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Fetchini.MODID);
-    public static final RegistryObject<CreativeModeTab> ITEMS_TAB=CREATIVE_MODE_TABS.register("items_tab",()-> CreativeModeTab.builder().icon(()->new ItemStack(ModItems.SAPPHIRE.get()))
+    public static final RegistryObject<CreativeModeTab> FETCHINI_ITEMS_TAB=CREATIVE_MODE_TABS.register("fetchini_items_tab",()-> CreativeModeTab.builder().icon(()->new ItemStack(ModItems.SAPPHIRE.get()))
             .title(Component.translatable("creativetab.items_tab"))
             .displayItems((pParameters, pOutput) -> {
                 pOutput.accept(ModItems.SAPPHIRE.get());
