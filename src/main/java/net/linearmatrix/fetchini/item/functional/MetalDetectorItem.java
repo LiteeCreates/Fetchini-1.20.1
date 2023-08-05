52019bb85f7d6f285e7c22d3747a7057c13b3b76
LiteeCreates <motomart2018@gmail.com>
2023-08-05 21:48:25 +0700

    New blocks and items commit (tooltip)

--- src/main/java/net/linearmatrix/fetchini/item/functional/MetalDetectorItem.java
+++ src/main/java/net/linearmatrix/fetchini/item/functional/MetalDetectorItem.java
@@ -6,11 +6,17 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.linearmatrix.fetchini.block.ModBlocks;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties){
@@ -37,6 +43,12 @@ public class MetalDetectorItem extends Item {
         }
         pContext.getItemInHand().hurtAndBreak(1,pContext.getPlayer(),player->player.broadcastBreakEvent(player.getUsedItemHand()));
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.fetchini.metal_detector"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private void valuableCoors(BlockPos blockPos, Player player, Block block) {
